62606700ac51f2d56ccf86bca216db818821a1bc
unix-axwell <30478004+unix-axwell@users.noreply.github.com>
2025-05-02 13:17:18 +0200

    Update run.sh
    - Changed configuration to install "nala" first and then use it to install the packages (making use of the history function).
    
    - Added tmux, btop and neofetch*
    
    - Added aliases for easy tmux access and custom update/upgrade/install commands.
    
    * I might replace neofetch with something else but for the moment it's also available in my repo.

--- run.sh
+++ run.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
-#Author: Radu - ASIR
+#Author: Radu - ASIR - I.E.S. Comercio
 
 #Update package list
 sudo apt update
 
-#Package installs
-sudo apt install -y tldr mc ranger ncdu
+#Nala setup
+sudo apt install -y nala
+
+#Packages Install
+sudo nala install -y tldr mc ranger ncdu tmux btop neofetch
+
+#Bash configuration
+echo "alias ttn='tmux new -s' >> ~/.bashrc"
+echo "alias tta='tmux attach -t' >> ~/.bashrc"
+echo "alias sysupdate='sudo nala update' >> ~/.bashrc"
+echo "alias sysupgrade='sudo nala upgrade' >> ~/.bashrc"
+echo "alias sysinstall='sudo nala install' >> ~/.bashrc"
+echo "alias syshistory='nala history' >> ~/.bashrc"
+echo "alias syslist='sudo apt list --upgradable' >> ~/.bashrc"
 
 #TLDR resources update
 tldr --update
