9a86eb1758b05c0f14890d1cb6ae088ffa7cb30b
unix-axwell <30478004+unix-axwell@users.noreply.github.com>
2025-05-07 08:06:59 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -10,17 +10,24 @@ sudo apt install -y nala
 #Packages Install
 sudo nala install -y tldr mc ranger ncdu tmux btop neofetch
 
-#Bash configuration
-echo "alias ttn='tmux new -s' >> ~/.bashrc"
-echo "alias tta='tmux attach -t' >> ~/.bashrc"
-echo "alias sysupdate='sudo nala update' >> ~/.bashrc"
-echo "alias sysupgrade='sudo nala upgrade' >> ~/.bashrc"
-echo "alias sysinstall='sudo nala install' >> ~/.bashrc"
-echo "alias syshistory='nala history' >> ~/.bashrc"
-echo "alias syslist='sudo apt list --upgradable' >> ~/.bashrc"
-
-#TLDR resources update
+#TLDR Resources Update
 tldr --update
+
+#Bash configuration
+echo "# === Custom Alias List ===" >> ~/.bashrc
+echo "alias ttn='tmux new -s'" >> ~/.bashrc
+echo "alias tta='tmux attach -t'" >> ~/.bashrc
+echo "alias sysupdate='sudo nala update'" >> ~/.bashrc
+echo "alias sysupgrade='sudo nala upgrade'" >> ~/.bashrc
+echo "alias sysinstall='sudo nala install'" >> ~/.bashrc
+echo "alias syshistory='nala history'" >> ~/.bashrc
+echo "alias syslist='sudo apt list --upgradable'" >> ~/.bashrc
+echo "alias syshold='sudo apt hold'" >> ~/.bashrc
+echo "alias sysrelease='sudo apt-mark unhold'" >> ~/.bashrc
+echo "# === Custom Alias List ===" >> ~/.bashrc
+
+#Resetting Bash
+source ~/.bashrc
 
 #Print message
 echo "Process Complete!"
